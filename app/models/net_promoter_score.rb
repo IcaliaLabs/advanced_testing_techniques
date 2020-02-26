6a5c82bf9dedb8e660a16d678eb03a10ca89c52f
Abraham Kuri <kurenn@icalialabs.com>
2020-02-26 04:04:55 +0000

    Adds barebones for net promoter score specs

--- app/models/net_promoter_score.rb
+++ app/models/net_promoter_score.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class NetPromoterScore < ApplicationRecord
+  validates :score, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 10
+  }
 end
--- spec/models/net_promoter_score_spec.rb
+++ spec/models/net_promoter_score_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe NetPromoterScore, type: :model do
+  it do
+    expect(subject).to validate_numericality_of(:score)
+      .is_greater_than_or_equal_to(0)
+      .is_less_than_or_equal_to(10)
+  end
 end
