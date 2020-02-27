9e6354ff90c08e737cb5563256d9b9635535f888
Abraham Kuri <kurenn@icalialabs.com>
2020-02-27 21:31:00 +0000

    Refactors the product spec file with a page object for product creation

--- spec/system/product_spec.rb
+++ spec/system/product_spec.rb
@@ -2,33 +2,57 @@
 
 require 'rails_helper'
 
+module Page
+  class Product
+    include Rails.application.routes.url_helpers
+    include Capybara::DSL
+
+    def initialize(name)
+      @name = name
+    end
+
+    def create
+      visit new_product_path
+      fill_in :product_name, with: @name 
+      fill_in :product_description, with: 'description'
+      fill_in :product_price, with: 500
+      click_button 'Create'
+    end
+
+    def exists?
+      has_css?('p.product_title', text: "Draft - #{@name}")
+    end
+  end
+end
+
 RSpec.describe 'Product', type: :system do
   context 'Product creation' do
     it 'creates the product' do
-      visit new_product_path
-      fill_in :product_name, with: 'Xbox One'
-      fill_in :product_description, with: 'It is so fun to play with this!'
-      fill_in :product_price, with: 500
-      click_button 'Create'
+      product_name = 'Xbox One'
+      page_product = Page::Product.new(product_name)
+      page_product.create
 
-      expect(page).to(
-        have_css('p.product_title', text: "Draft - Xbox One")
-      )
+      expect(page_product).to exist
     end
+  end
+
+  def create_product(product_name)
+    visit new_product_path
+    fill_in :product_name, with: product_name
+    fill_in :product_description, with: 'description'
+    fill_in :product_price, with: 500
+    click_button 'Create'
   end
 
   context 'Product publication' do
     it 'cancels the product' do
-      visit new_product_path
-      fill_in :product_name, with: 'Nintendo Switch'
-      fill_in :product_description, with: 'It is a console more for the young ones!'
-      fill_in :product_price, with: 300
-      click_button 'Create'
+      product_name = 'Nintendo Switch'
+      create_product product_name
 
       click_button 'Publish'
 
       expect(page).to(
-        have_css('p.product_title', text: 'Published - Nintendo Switch')
+        have_css('p.product_title', text: "Published - #{product_name}")
       )
     end
   end
