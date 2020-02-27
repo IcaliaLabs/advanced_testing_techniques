f0b7aed17f62b81213040718f1459fa49445c675
Abraham Kuri <kurenn@icalialabs.com>
2020-02-27 19:35:48 +0000

    Adds product scaffold

--- db/migrate/20200227193253_create_products.rb
+++ db/migrate/20200227193253_create_products.rb
@@ -2,8 +2,8 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.float :price
       t.text :description
+      t.float :price
 
       t.timestamps
     end
