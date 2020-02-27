cce6a9bfdc606703c37ed40290bc84dfdaa224c2
Abraham Kuri <kurenn@icalialabs.com>
2020-02-27 19:41:37 +0000

    Adds product system spec setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  enum status: {
+    draft: 0,
+    published: 1,
+    cancelled: 2
+  }
 end
---
+++ spec/system/product_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Product', type: :system do
+  context 'Product creation' do
+    pending
+  end
+
+  context 'Product cancellation' do
+    pending
+  end
+
+  context 'Product publication' do
+    pending
+  end
+end
