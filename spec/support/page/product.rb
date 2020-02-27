d404cd5978de559d2f776769fd8f9e2c49f1b37d
Abraham Kuri <kurenn@icalialabs.com>
2020-02-27 21:42:13 +0000

    Moves the page class definition to a file

---
+++ spec/support/page/product.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Page
+  class Product
+    include Rails.application.routes.url_helpers
+    include Capybara::DSL
+
+    def initialize(name)
+      @name = name
+    end
+
+    def create
+      visit new_product_path
+      fill_in :product_name, with: @name
+      fill_in :product_description, with: 'description'
+      fill_in :product_price, with: 500
+      click_button 'Create'
+    end
+
+    def exists?
+      product_title.has_text? "Draft - #{@name}"
+    end
+
+    def publish
+      click_button 'Publish'
+    end
+
+    def published?
+      product_title.has_text? "Published - #{@name}"
+    end
+
+    private
+
+    def product_title
+      find('p.product_title')
+    end
+  end
+end
