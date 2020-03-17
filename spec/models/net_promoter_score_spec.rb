4f211677eec6a671ba082632ef55729ed2694b57
Alberto Antillon Abrego <champi@icalialabs.com>
2020-03-17 11:42:02 -0600

    adding detractor test to exlude non-detractor records on detractor scope

--- spec/models/net_promoter_score_spec.rb
+++ spec/models/net_promoter_score_spec.rb
@@ -116,5 +116,12 @@ RSpec.describe NetPromoterScore, type: :model do
         expect(NetPromoterScore.detractor).to include(net_promoter_score)
       end
     end
+
+    it 'excludes records with scores not within detractor range' do
+      (7..10).each do |score|
+        net_promoter_score = create :net_promoter_score, score: score
+        expect(NetPromoterScore.detractor).to_not include(net_promoter_score)
+      end
+    end
   end
 end
